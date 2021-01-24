831e440c8432a6ef5f4eaa85d505528aa903aa6c
mdresch <m.drescher@cbaconsultancy.nl>
2021-01-24 06:49:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# why-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/why-react)
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Why Use and Learn React</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <p>Book: jscomplete.com/beginning.js</p>
+      <p>FAQ on Reacht: https jscomplete.com/react-cfg</p>
+
+      <p>React is a User Interface Library not a complete solution (not a framework)</p>
+      <p>Small labrirary - Virtual Browser VS dom API</p>
+      <div>Just JavaScript</div>
+      <p>Reacte Native for the win</p>
+      <p>Battle Tested</p>
+      <p>Declarative language (model UI and state)</p>
+      <p>jscomplete.com/why-react</p>
+
+    </div>
+  );
+}
